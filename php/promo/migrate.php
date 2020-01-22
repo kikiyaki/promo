ce37c3426d99727f4d345ff803fbfb8128a08982
kikiyaki <krl9628@gmail.com>
2020-01-23 01:50:43 +0300

    migrate and migrations

--- php/promo/migrate.php
+++ php/promo/migrate.php
@@ -1,9 +1,21 @@
 <?php
 
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+/**
+ * Make database migration
+ */
 
 require_once __DIR__ . '/MyPDO.php';
 
 $pdo = new MyPDO();
+
+$migrationFiles = glob(__DIR__ . '/migrations/*.sql');
+foreach ($migrationFiles as $migrationFile) {
+    $fileName = basename($migrationFile);
+    $query = file_get_contents($migrationFile);
+    try {
+        $pdo->exec($query);
+        echo "Migration completed: $fileName";
+    } catch (Throwable $e) {
+        echo "Error migration: $fileName \n";
+    }
+}
