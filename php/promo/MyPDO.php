97218c1a81108bd44f40a91f560fb4bd6297c502
kikiyaki <krl9628@gmail.com>
2020-01-22 10:26:16 +0300

    Create mirate and pdo

---
+++ php/promo/MyPDO.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class MyPDO extends PDO
+{
+
+    public function __construct()
+    {
+        $host = 'pgsql';
+        $db = 'promo';
+        $user = 'promo';
+        $pass = 'promo';
+
+        $dsn = "pgsql:host=$host;dbname=$db";
+
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+
+        parent::__construct($dsn, $user, $pass, $options);
+    }
+}
