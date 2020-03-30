e0fb8dacf6d2b1758993d9d96ed4d4fec607d2f6
kikiyaki <krl9628@gmail.com>
2020-03-30 10:14:47 +0300

    i24 unique user key creating

--- php/promo/src/CreateAction.php
+++ php/promo/src/CreateAction.php
@@ -21,25 +21,31 @@ class CreateAction extends ActionStd
 
     public function handle()
     {
+        $randomKey = hash('sha256', rand(0, 999999999));
+
         $query =
 <<<SQL
-INSERT INTO users (data)
+INSERT INTO users (data, key)
     VALUES
     ('
         {
         "my":{},
         "forMe":{}
          }
-    ')
-    RETURNING id;
+    ',
+     '$randomKey')
+    RETURNING id, key;
 SQL;
 
         $pdo = new MyPDO();
         $return = $pdo->query($query)->fetch();
         $newUserId = $return['id'];
+        $newUserKey = $return['key'];
 
         $this->responseData->setStatus(ResponseData::SUCCESS);
         $this->responseData->setData('NEW_USER_ID', $newUserId);
+        $this->responseData->setData('NEW_USER_KEY', $newUserKey);
+
         return $this->responseData->stringify();
     }
 }
